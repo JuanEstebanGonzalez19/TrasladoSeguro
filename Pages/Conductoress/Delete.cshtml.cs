9722973c241d633d7b1502b8f26282555a5c1b35
JuanEstebanGonzalez19 <juanestebangonzalezsierra0327@gmail.com>
2023-05-27 15:15:16 -0500

    Resto

---
+++ Pages/Conductoress/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TrasladoSeguro.Pages.Conductoress
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
